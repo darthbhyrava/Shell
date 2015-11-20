056629d7b5576099ac7adf724d895b3f0667b61d
darthbhyrava <hbhyrava@gmail.com>
2015-11-20 13:13:55 +0530

    Read and parse commands

--- brennsh/lsh.c
+++ brennsh/lsh.c
@@ -9,6 +9,7 @@ char *lsh_read_line()
 	char *buffer = malloc(sizeof(char)*bufsize);
 	int c;
 
+	//Check for allocation errors
 	if(!buffer) {
 		fprintf(stderr, "lsh:allocation error!\n");
 		exit(EXIT_FAILURE);
@@ -16,7 +17,9 @@ char *lsh_read_line()
 
 	while(1)
 	{
+		//read a character (and store it in 'int', not 'char', since EOF is int)
 		c=getchar();
+		//Check if we've reached End of File or new line (this shell cannot take multi-line commands)
 		if ( c==EOF || c=='\n')
 		{
 			buffer[position]='\0';
@@ -24,10 +27,10 @@ char *lsh_read_line()
 		} 
 		else
 		{
-			buffer[position]=c;
+			buffer[position++]=c;
 		}
-		position++;
-
+		
+		//Check if buffer size is insufficient, if yes, then allocate more memory 
 		if(position>=bufsize)
 		{
 			bufsize+=LSH_RL_BUFSIZE;
@@ -39,6 +42,56 @@ char *lsh_read_line()
 			}
 		}
 	}
+
+	//This function could have been implemented in a much simpler way using the getline() function in stdio.h
+	//char *lsh_read_line()
+	//{
+	//	char *line = NULL;
+	//	ssize_t bufsize = 0; //getline allocates buffer :P
+	//	getline(&line, &bufsize, stdin);
+	//	return line;
+	//}
+}
+
+//Function to split the command into components
+//Not allowing quoting or backslash esaping
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char *line)
+{
+	int bufsize = LSH_TOK_BUFSIZE, position=0;
+	//Dynamically initiating an array of pointers instead of an array of characters like in read_line()
+	char **tokens = malloc(sizeof(char*)*bufsize);
+	char *token;
+
+	//Allocation error
+	if(!tokens)
+	{
+		frpintf(stderr, "lsh: allocation error!\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	//strtok() tokenizes the string; it returns pointers to within the string, and places a \0 at the end of each token.
+	//We store each pointer in an array of pointers 
+	token = strtok(line, LSH_TOK_DELIM);
+	while(token!==NULL)
+	{
+		tokens[position]=token;
+		position++;
+		if(position >= bufsize) 
+		{
+			bufsize+=LSH_TOK_BUFSIZE;
+			tokens=realloc(tokens, sizeof(char*)*bufsize);
+			if(!tokens)
+			{
+				fprintf(stderr, "lsh: allocation error!\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token=strtok(NULL, LSH_TOK_DELIM);
+	}
+	tokens[position]=NULL;
+	return tokens;
 }
 
 //Command Loop
